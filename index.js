902ae85d3a0fcded66b0266e23a5d01d391d7562
Thestickman391 <stickman391@gmail.com>
2021-01-13 17:06:37 +1300

    Format json and search through it

--- index.js
+++ index.js
@@ -2,6 +2,36 @@ const fs = require("fs");
 const ufsd = require("./MiidataSwi");
 const KaitaiStream = require('kaitai-struct/KaitaiStream');
 
-const fileContent = fs.readFileSync("defaultM.ufsd");
-const parsedUfsd = new ufsd(new KaitaiStream(fileContent));
-console.log(parsedUfsd);
+function getStringLocation(array, string) {
+    for( var i = 0; i < array.length; i++ ) {
+        for( var j = 0; i < array[i].length; j++ ) {
+            for( var k = 0; i < array[i][j].length; k++ ) {
+                if(array[i][j][k] === string) {
+                    return {
+                        first: i,
+                        second: j,
+                        third: k,
+                    };
+                }
+            }
+        }
+    }
+}
+
+const defaultM = fs.readFileSync("defaultM.ufsd");
+const parsedDefaultM = new ufsd(new KaitaiStream(defaultM));
+// console.log(parsedDefaultM);
+
+const defaultF = fs.readFileSync("defaultF.ufsd");
+const parsedDefaultF = new ufsd(new KaitaiStream(defaultF));
+// console.log(parsedDefaultF);
+
+const nina = fs.readFileSync("Nina.ufsd");
+const parsedNina = new ufsd(new KaitaiStream(nina));
+console.log(parsedNina);
+
+const map = fs.readFileSync("maps_Switch.json");
+const parsedmap = JSON.parse(map);
+// console.log(parsedmap.hair[0][0]);
+console.log(getStringLocation(parsedmap.hair, '0x56'));
+
