2d764b9ad850e22158e86d0f897ecb5bc7558b6f
Thestickman391 <stickman391@gmail.com>
2021-01-13 18:20:40 +1300

    Functions to generate default eyebrow rotation

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const KaitaiStream = require('kaitai-struct/KaitaiStream');
 function getStringLocation(array, string) {
     for( var i = 0; i < array.length; i++ ) {
         for( var j = 0; j < array[i].length; j++ ) {
-            if(array[i].length > 1) {
+            if(array[i][j].isArray)
+            {
                 for( var k = 0; k < array[i][j].length; k++ ) {
                     if(array[i][j][k] === string) {
                         return {
@@ -14,6 +15,14 @@ function getStringLocation(array, string) {
                             column: k + 1,
                         };
                     }
+                }
+            }
+            else {
+                if(array[i][j] === string) {
+                    return {
+                        row: i + 1,
+                        column: j + 1,
+                    };
                 }
             }
         }
@@ -26,6 +35,21 @@ function pad(n, width, z) {
     return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
+const rotation = fs.readFileSync("rotation.json");
+const parsedrotation = JSON.parse(rotation);
+
+function getEyebrowRotation(eyebrowType) {
+    if (eyebrowType === '0x17') {
+        return null;
+    } else {
+        return getStringLocation(parsedrotation.eyebrows, eyebrowType).row + 3;
+    }
+}
+
+function getEyeRotation(eyeType) {
+    return getStringLocation(parsedrotation.eyes, eyeType).row + 2;
+}
+
 const defaultM = fs.readFileSync("defaultM.ufsd");
 const parsedDefaultM = new ufsd(new KaitaiStream(defaultM));
 // console.log(parsedDefaultM);
@@ -36,11 +60,15 @@ const parsedDefaultF = new ufsd(new KaitaiStream(defaultF));
 
 const nina = fs.readFileSync("Nina.ufsd");
 const parsedNina = new ufsd(new KaitaiStream(nina));
-console.log(parsedNina);
+// console.log(parsedNina);
 // console.log(parsedNina.eyebrowType.toString(16));
 
 const map = fs.readFileSync("maps_Switch.json");
 const parsedmap = JSON.parse(map);
 // console.log(parsedmap.hair[0][0][0]);
-// console.log(getStringLocation(parsedmap.hair, '0x' + pad(parsedNina.hairType.toString(16), 2)));
-
+// console.log(getStringLocation(parsedmap.eyebrows, '0x' + pad(parsedNina.eyebrowType.toString(16), 2)));
+// console.log(getEyebrowRotation('0x' + pad(parsedNina.eyebrowType.toString(16), 2)));
+console.log(getEyeRotation('0x' + pad(parsedNina.eyeType.toString(16), 2)));
+// console.log('0x' + pad(parsedNina.eyebrowType.toString(16), 2));
+// console.log(getStringLocation(parsedrotation.eyebrows, "0x01"));
+// console.log(parsedrotation.eyebrows[0].length);
