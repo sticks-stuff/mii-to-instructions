f19bbd196409e691326bd99247ff2f2224164574
Thestickman391 <stickman391@gmail.com>
2021-01-13 17:21:55 +1300

    Converting to hex

--- index.js
+++ index.js
@@ -4,18 +4,26 @@ const KaitaiStream = require('kaitai-struct/KaitaiStream');
 
 function getStringLocation(array, string) {
     for( var i = 0; i < array.length; i++ ) {
-        for( var j = 0; i < array[i].length; j++ ) {
-            for( var k = 0; i < array[i][j].length; k++ ) {
-                if(array[i][j][k] === string) {
-                    return {
-                        first: i,
-                        second: j,
-                        third: k,
-                    };
+        for( var j = 0; j < array[i].length; j++ ) {
+            if(array[i].length > 1) {
+                for( var k = 0; k < array[i][j].length; k++ ) {
+                    if(array[i][j][k] === string) {
+                        return {
+                            page: i + 1,
+                            row: j + 1,
+                            column: k + 1,
+                        };
+                    }
                 }
             }
         }
     }
+}
+
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
 const defaultM = fs.readFileSync("defaultM.ufsd");
@@ -28,10 +36,11 @@ const parsedDefaultF = new ufsd(new KaitaiStream(defaultF));
 
 const nina = fs.readFileSync("Nina.ufsd");
 const parsedNina = new ufsd(new KaitaiStream(nina));
-console.log(parsedNina);
+// console.log(parsedNina.eyebrowType);
+// console.log(parsedNina.eyebrowType.toString(16));
 
 const map = fs.readFileSync("maps_Switch.json");
 const parsedmap = JSON.parse(map);
-// console.log(parsedmap.hair[0][0]);
-console.log(getStringLocation(parsedmap.hair, '0x56'));
+// console.log(parsedmap.hair[0][0][0]);
+console.log(getStringLocation(parsedmap.hair, '0x' + pad(parsedNina.hairType.toString(16), 2)));
 
