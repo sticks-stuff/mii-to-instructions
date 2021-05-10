89d73398ac7167288c3f7e89f5ecf74bb2e61bfd
i'm alice <48461034+HEYimHeroic@users.noreply.github.com>
2021-05-10 03:13:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function getEyebrowRotation(eyebrowType) {
     if (eyebrowType === '0x17') {
         return 0;
     } else {
-        return getStringLocation(parsedrotation.eyebrows, eyebrowType).row + 4;
+        return getStringLocation(parsedrotation.eyebrows, eyebrowType).row + 3;
     }
 }
 
